28bd30bf319fb0216603bfce3c5bfc7798df8adc
jborruel308@me.com <jborruel308@me.com>
2024-09-19 19:52:34 -0700

    add navbar partial add newapp

---
+++ controllers/applications.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+router.get('/', async (req, res) => {
+    try {
+      res.render('applications/index.ejs');
+    } catch (error) {
+      console.log(error)
+      res.redirect('/')
+    }
+  });
+  router.get('/new', async (req, res) => {
+    res.render('applications/new.ejs');
+  });
+  
+
+module.exports = router;
