d24f29302634686e387e5be83815957a2e0a7854
jborruel308@me.com <jborruel308@me.com>
2024-09-19 20:16:27 -0700

    add applciation show, new and index routes and delete button

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,15 +6,44 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {
-      res.render('applications/index.ejs');
+      const currentUser = await User.findById(req.session.user._id);
+      res.render('applications/index.ejs', {
+        applications: currentUser.applications,
+      })
     } catch (error) {
       console.log(error)
       res.redirect('/')
     }
   });
+
   router.get('/new', async (req, res) => {
     res.render('applications/new.ejs');
   });
-  
 
-module.exports = router;
+  router.get('/:applicationId', async (req, res) => {
+    try {
+     
+      const currentUser = await User.findById(req.session.user._id);
+      const application = currentUser.applications.id(req.params.applicationId);
+      res.render('applications/show.ejs', {
+        application: application,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+
+
+  router.post('/', async (req,res) => {
+    try{
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.applications.push(req.body);
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+
